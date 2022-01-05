5f60fe542936d4a6de7f9ea8c8bd12767da25ee9
Anh Tong <tongkimanh@yahoo.com>
2022-01-05 04:27:34 +0000

    To complete exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,30 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+store1 = Store.new do |s|
+  s.name = "Burnaby"
+  s.annual_revenue = 300000
+  s.mens_apparel = true
+  s.womens_apparel = true
+end
+store1.save
+
+store2 = Store.new do |s|
+  s.name = "Richmond"
+  s.annual_revenue = 1260000
+  s.mens_apparel = false
+  s.womens_apparel = true
+end
+store2.save
+
+store3 = Store.new do |s|
+  s.name = "Gastown"
+  s.annual_revenue = 190000
+  s.mens_apparel = true
+  s.womens_apparel = false
+end
+store3.save
+
+stores = Store.all
+puts "*****************************************************"
+puts stores.count
