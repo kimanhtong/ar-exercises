7cd61ac94e89a7458e6d84d83b796d9ed1dc7704
Anh Tong <tongkimanh@yahoo.com>
2022-01-05 06:45:02 +0000

    To complete exercise 6

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,29 +7,29 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
-store4 = Store.new do |s|
+@store4 = Store.new do |s|
   s.name = "Surrey"
   s.annual_revenue = 224000
   s.mens_apparel = false
   s.womens_apparel = true
 end
-store4.save
+@store4.save
 
-store5 = Store.new do |s|
+@store5 = Store.new do |s|
   s.name = "Whistler"
   s.annual_revenue = 1900000
   s.mens_apparel = true
   s.womens_apparel = false
 end
-store5.save
+@store5.save
 
-store6 = Store.new do |s|
+@store6 = Store.new do |s|
   s.name = "Yaletown"
   s.annual_revenue = 430000
   s.mens_apparel = true
   s.womens_apparel = true
 end
-store6.save
+@store6.save
 
 @mens_stores = Store.where(mens_apparel:true).where(womens_apparel:false)
 @mens_stores.each do |s|
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+Store.has_many:employees
+Employee.belongs_to:store
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store4.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store5.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store6.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+stores = Store.all
+for s in stores
+  puts "#{s.name} has #{s.employees.count} employee(s)"
+end
