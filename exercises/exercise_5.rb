e554be92157c647a14b76418dd689323a8d1b776
Anh Tong <tongkimanh@yahoo.com>
2022-01-05 06:11:56 +0000

    To complete exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -1,10 +1,14 @@
 require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
+#require_relative './exercise_1'
+#require_relative './exercise_2'
+#require_relative './exercise_3'
 require_relative './exercise_4'
 
 puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Total revenue of all store: #{Store.sum("annual_revenue")}"
+puts "Average revenue of all store: #{Store.average("annual_revenue")}"
+n = Store.where("annual_revenue >= ?", "1000000").count
+puts "Number of stores that are generating $1M or more in annual sales: #{n}"
